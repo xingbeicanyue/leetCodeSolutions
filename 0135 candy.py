dcbe9df0704d0e80fffa81d63b60ead87953f8ca
XBCY <809382461@qq.com>
2020-12-25 01:37:58 +0800

    feat: #135, #455, #1286

---
+++ 0135 candy.py
@@ -0,0 +1,68 @@
+"""
+老师想给孩子们分发糖果，有 N 个孩子站成了一条直线，老师会根据每个孩子的表现，预先给他们评分。
+你需要按照以下要求，帮助老师给这些孩子分发糖果：
+* 每个孩子至少分配到 1 个糖果。
+* 相邻的孩子中，评分高的孩子必须获得更多的糖果。
+
+那么这样下来，老师至少需要准备多少颗糖果呢？
+
+示例 1:
+输入: [1,0,2]
+输出: 5
+解释: 你可以分别给这三个孩子分发 2、1、2 颗糖果。
+
+示例 2:
+输入: [1,2,2]
+输出: 4
+解释: 你可以分别给这三个孩子分发 1、2、1 颗糖果。
+     第三个孩子只得到 1 颗糖果，这已满足上述两个条件。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/candy
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+标签：贪心算法
+"""
+
+from typing import List
+
+
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        if len(ratings) <= 1:
+            return len(ratings)
+        result = 1
+        lastValue = 1  # 上一个孩子获得的糖果数
+        descendStartIdx = -1  # 单调递减区间开始下标，负数表示当前不是单调递减区间
+        descendStartValue = -1  # 单调递减区间开始的糖果数
+        for i in range(1, len(ratings)):
+            curTrend = ratings[i] - ratings[i - 1]
+            if curTrend == 0:
+                lastValue = 1
+                result += lastValue
+                descendStartIdx = -1
+            elif curTrend > 0:
+                lastValue += 1
+                result += lastValue
+                descendStartIdx = -1
+            else:
+                if descendStartIdx < 0:
+                    descendStartIdx = i - 1
+                    descendStartValue = lastValue
+                if lastValue == 1:  # 需要降低但已无法降低，需增加当前递减区间的每个值（除了首项）
+                    result += (i - descendStartIdx)
+                    if descendStartValue - 1 < i - descendStartIdx:  # 递减区间的首项也需增加
+                        result += 1
+                        descendStartValue += 1
+                else:
+                    lastValue = 1
+                    result += lastValue
+        return result
+
+
+if __name__ == '__main__':
+    s = Solution()
+    r = s.candy([1, 0, 2])
+    print(r)
+    r = s.candy([1, 2, 2])
+    print(r)
---
+++ 1286 iteratorForCombination.py
@@ -0,0 +1,64 @@
+"""
+请你设计一个迭代器类，包括以下内容：
+* 一个构造函数，输入参数包括：
+  一个有序且字符唯一的字符串 characters（该字符串只包含小写英文字母）和一个数字 combinationLength 。
+* 函数 next() ，按字典序返回长度为 combinationLength 的下一个字母组合。
+* 函数 hasNext() ，只有存在长度为 combinationLength 的下一个字母组合时，才返回 True；否则，返回 False。
+ 
+示例：
+CombinationIterator iterator = new CombinationIterator("abc", 2); // 创建迭代器 iterator
+iterator.next(); // 返回 "ab"
+iterator.hasNext(); // 返回 true
+iterator.next(); // 返回 "ac"
+iterator.hasNext(); // 返回 true
+iterator.next(); // 返回 "bc"
+iterator.hasNext(); // 返回 false
+ 
+提示：
+* 1 <= combinationLength <= characters.length <= 15
+* 每组测试数据最多包含 10^4 次函数调用。
+* 题目保证每次调用函数 next 时都存在下一个字母组合。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/iterator-for-combination
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+标签：设计、回溯算法
+"""
+
+
+class CombinationIterator:
+
+    def __init__(self, characters: str, combinationLength: int):
+        self._characters = characters
+        self._length = combinationLength
+        self._characterIdxs = [i for i in range(combinationLength)]
+
+    def next(self) -> str:
+        result = ''.join(self._characters[idx] for idx in self._characterIdxs)
+        self._doIter()
+        return result
+
+    def hasNext(self) -> bool:
+        return self._characterIdxs is not None
+
+    def _doIter(self):
+        """ 迭代至下一个值 """
+        while self._characterIdxs:
+            nextIdx = self._characterIdxs.pop() + 1
+            characterLackCount = self._length - len(self._characterIdxs)
+            if len(self._characters) - nextIdx >= characterLackCount:
+                self._characterIdxs += [i for i in range(nextIdx, nextIdx + characterLackCount)]
+                return
+        else:
+            self._characterIdxs = None
+
+
+if __name__ == '__main__':
+    iterator = CombinationIterator('abc', 2)
+    print(iterator.next())
+    print(iterator.hasNext())
+    print(iterator.next())
+    print(iterator.hasNext())
+    print(iterator.next())
+    print(iterator.hasNext())
