ac443709915bdcd2d9fd11876514b8ba1bf3d619
XBCY <809382461@qq.com>
2021-01-04 02:19:08 +0800

    feat: #5, #214, #509

---
+++ 0005 longestPalindromicSubstring.py
@@ -0,0 +1,52 @@
+"""
+给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+
+示例 1：
+输入: "babad"
+输出: "bab"
+注意: "aba" 也是一个有效答案。
+
+示例 2：
+输入: "cbbd"
+输出: "bb"
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/longest-palindromic-substring
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+标题：字符串、动态规划
+"""
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        # Manacher算法
+        s = '#' + '#'.join(s) + '#'
+        resultLen, resultSIdx = -1, -1
+        radius = []  # 每个下标为中心的臂长
+        lastPalindromeCenterIdx, lastPalindromeEndIdx = -1, -1  # 最右回文中心|右端下标
+        for i, c in enumerate(s):
+            if i < lastPalindromeEndIdx:
+                startRadius = min(radius[lastPalindromeCenterIdx * 2 - i], lastPalindromeEndIdx - i) + 1
+            else:
+                startRadius = 1
+            leftIdx, rightIdx = i - startRadius, i + startRadius
+            while leftIdx >= 0 and rightIdx < len(s) and s[leftIdx] == s[rightIdx]:
+                leftIdx -= 1
+                rightIdx += 1
+            leftIdx, rightIdx, curLen = leftIdx + 1, rightIdx - 1, rightIdx - leftIdx - 2
+
+            if rightIdx > lastPalindromeEndIdx:
+                lastPalindromeCenterIdx, lastPalindromeEndIdx = i, rightIdx
+            radius.append(curLen // 2)
+            if curLen > resultLen:
+                resultLen, resultSIdx = curLen, leftIdx
+        return ''.join(s[i] for i in range(resultSIdx, resultSIdx + resultLen + 1) if i % 2 == 1)
+
+
+if __name__ == '__main__':
+    s = Solution()
+    r = s.longestPalindrome('babad')
+    print(r)
+    r = s.longestPalindrome('cbbd')
+    print(r)
---
+++ 0214 shortestPalindrome.py
@@ -0,0 +1,54 @@
+"""
+给定一个字符串 s，你可以通过在字符串前面添加字符将其转换为回文串。找到并返回可以用这种方式转换的最短回文串。
+
+示例 1：
+输入：s = "aacecaaa"
+输出："aaacecaaa"
+
+示例 2：
+输入：s = "abcd"
+输出："dcbabcd"
+
+提示：
+* 0 <= s.length <= 5 * 104
+* s 仅由小写英文字母组成
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/shortest-palindrome
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+标签：字符串
+"""
+
+
+class Solution:
+    def shortestPalindrome(self, s: str) -> str:
+        # 类似KMP，对s的逆序列匹配s，求最后的匹配
+        # 计算nexts
+        nexts, nextIdx = [0] * (len(s) + 1), 0
+        for i in range(1, len(s)):
+            if s[i] == s[nextIdx]:
+                nexts[i + 1] = nextIdx = nextIdx + 1
+            else:
+                nexts[i + 1] = nextIdx = int(s[i] == s[0])
+        # 匹配
+        reversedS = s[::-1]
+        sIdx, rsIdx = 0, 0
+        while rsIdx < len(reversedS):
+            if s[sIdx] == reversedS[rsIdx]:
+                sIdx += 1
+                rsIdx += 1
+            elif sIdx == 0:
+                rsIdx += 1
+            else:
+                sIdx = nexts[sIdx]
+        # 添加前缀并返回
+        return reversedS[0: len(reversedS) - sIdx] + s
+
+
+if __name__ == '__main__':
+    s = Solution()
+    r = s.shortestPalindrome('aacecaaa')
+    print(r)
+    r = s.shortestPalindrome('abcd')
+    print(r)
